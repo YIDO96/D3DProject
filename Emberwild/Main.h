3d5d7ed8bcf0ba21bab087f524cc62f7a3cee2ec
VecTor <lsh961101@naver.com>
2024-08-25 18:18:56 +0900

    123

--- Emberwild/Main.h
+++ Emberwild/Main.h
@@ -5,16 +5,14 @@ class Main : public Scene
 private:
 	Camera* cam1;
 	Grid* grid;
-
-
-
+	
 public:
 	Main();
 	~Main();
 	virtual void Init() override;
-	virtual void Release() override; //?´ì œ
+	virtual void Release() override; //ÇØÁ¦
 	virtual void Update() override;
-	virtual void LateUpdate() override;//ê°±ì‹ 
+	virtual void LateUpdate() override;//°»½Å
 	virtual void PreRender() override;
 	virtual void Render() override;
 	virtual void ResizeScreen() override;
